5dbe0b94c238f3d339ed6e62ddd5baa106669e43
GetoarNishefci <getoar.n1@gmail.com>
2025-08-26 13:14:48 +0200

    feat:add new features sizes and colors

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
@@ -0,0 +1,23 @@
+import prismadb from "@/lib/prismadb";
+import SizeFrom from "./components/size-form";
+
+const SizesPage = async({params}:{
+    params:{sizeId:string}
+}) =>{
+
+    const Sizes = await prismadb.size.findUnique({
+        where:{
+            id:params.sizeId
+        }
+    });
+
+    return(
+            <div className="flex-col">
+                    <div className="flex-1 space-y-4 p-8 pt-6">
+                    <SizeFrom initialData={Sizes}/>
+                    </div>
+            </div>
+    );
+}
+
+export default SizesPage;
